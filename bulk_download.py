ee9f365079ca0f595a47e6c25b84f18e243c40fb
gmmkmtgk <keshavmaheshwari800@gmail.com>
2023-09-16 21:50:54 +0530

    Task Completed

--- bulk_download.py
+++ bulk_download.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 import webbrowser
 
 # Get the start and end dates from the user
@@ -12,15 +12,23 @@ end_date = datetime.strptime(end_date_str, "%Y-%m-%d")
 # Loop through the date range and open URLs in the web browser
 current_date = start_date
 while current_date <= end_date:
+    # Check if the day is Saturday (5) or Sunday (6)
+    if current_date.weekday() == 5 or current_date.weekday() == 6:
+        # Check if the month is October or November
+        if current_date.month not in [10, 11]:
+            # Skip weekends in other months
+            current_date += timedelta(days=1)
+            continue
+
     date_str = current_date.strftime("%d%b%Y").upper()
     year = current_date.strftime("%Y")
     month = current_date.strftime("%b").upper()
-    
+
     # Generate the dynamic URL
     url = f"https://nsearchives.nseindia.com/content/historical/EQUITIES/{year}/{month}/cm{date_str}bhav.csv.zip"
-    
+
     # Open the URL in the web browser
     webbrowser.open(url)
-    
+
     # Move to the next date
     current_date += timedelta(days=1)
