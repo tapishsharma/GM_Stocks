6aa8d6c8182876bb3a292e79104db0eaaaa84836
gmmkmtgk <keshavmaheshwari800@gmail.com>
2023-09-16 13:37:43 +0530

    Date added

--- data_manage.py
+++ data_manage.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+from datetime import datetime
 
 # Directory containing the CSV files
 directory = 'csv_files'
@@ -12,15 +13,19 @@ for filename in os.listdir(directory):
     if filename.endswith(".csv"):
         file_path = os.path.join(directory, filename)
         
+        # Extract the date part from the filename
+        date_str = filename[2:-8]  # Extracts the '01SEP2023' part
+        date_obj = datetime.strptime(date_str, "%d%b%Y")
+        
         # Read the CSV file into a DataFrame
         df = pd.read_csv(file_path)
         
         # Filter rows where 'series' is 'EQ'
         df = df[df['SERIES'] == 'EQ']
         
-        # Extract the 'SYMBOL' and 'CLOSE' columns and rename 'CLOSE' to the filename
+        # Extract the 'SYMBOL' and 'CLOSE' columns and rename 'CLOSE' to the date
         df = df[['SYMBOL', 'CLOSE']]
-        df.rename(columns={'CLOSE': filename}, inplace=True)
+        df.rename(columns={'CLOSE': date_obj.strftime("%Y-%m-%d")}, inplace=True)
         
         if not processed_first_file:
             merged_df = df
